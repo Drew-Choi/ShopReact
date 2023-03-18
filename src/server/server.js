20d8714243dcd37fb5be2df7bb8618bb3b656934
Drew <drc920630@gmail.com>
2023-03-18 23:30:21 +0900

    서버 구축 중

---
+++ src/server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const app = express();
+const PORT = 4000;
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: '1234',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 600,
+    },
+  }),
+);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(err.statusCode);
+  res.send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT}포트에서 서버 실행 중 입니다.`);
+});
