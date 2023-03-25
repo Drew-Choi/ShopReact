d818adea1bf7010e4bb160f4887cfbc5bde9a9a5
Drew <drc920630@gmail.com>
2023-03-25 14:35:03 +0900

    리덕스 변경함수 적용 학습 완료

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,13 +1,17 @@
 import { Table } from "react-bootstrap"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../store";
 
 function Cart() {
 
   let redux = useSelector((state) => state );
   let cartData = redux.cartData;
+  //state변경을 위해서 store.js에게 요청을 보내주는 함수
+  let dispatch = useDispatch();
 
   return (
     <div>
+      {redux.user}
       <Table>
         <thead>
           <tr>
@@ -19,16 +23,21 @@ function Cart() {
         </thead>
         <tbody>
           {
-            cartData.map((el) => {
-            return (
-            <tr>
+            cartData.map((el, num) => 
+             
+            <tr key={num}>
               <td>{el.id}</td>
               <td>{el.name}</td>
               <td>{el.count}</td>
-              <td>안녕</td>
+              <td><button onClick={() => {
+                //useDispatch를 사용해서 변수에 담고
+                //그 함수에 store에서 불러낸 setUser를 담아준다.
+                //그래야 setUser를 사용 할 수 있다.
+                dispatch(setUser())
+              }}>+</button></td>
             </tr>
-             )
-           })
+             
+           )
           }
         </tbody>
       </Table> 
--- src/store.js
+++ src/store.js
@@ -3,7 +3,19 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 let user = createSlice({
   name: 'user',
   initialState: ['kim', 'park'],
+  //리덕스에서 stat변경 방법 스텝 1. reducers키를 만들어라
+  //그리고 그 안에 작명하고 함수 하나 만들어라
+  reducers: {
+    setUser(state) {
+      return ['john', state];
+      //만약 바꿔치기가 아닌 추가로 하고 싶으면 setUser(state){}
+      //이렇게 매개변수 안에 state를 넣어주고 return ['john kim' + state] 이렇게 해준다.
+      //state매개변수는 기존 ['kim', 'park']을 유지한 것이고, 거기에 새로운 'john kim'을 넣어주는 것이다.
+    },
+  },
 });
+//setUser를 밖으로 빼내기, 구조분해할당으로 빼줌
+export let { setUser } = user.actions;
 
 let stock = createSlice({
   name: 'stock',
@@ -13,7 +25,7 @@ let stock = createSlice({
 let cartData = createSlice({
   name: 'cartData',
   initialState: [
-    { id: 0, name: 'White and Black', count: 2 },
+    { id: 1, name: 'White and Black', count: 2 },
     { id: 2, name: 'Grey Yordan', count: 1 },
   ],
 });
